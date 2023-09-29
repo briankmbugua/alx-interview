689309e11f66c7b064501af1f68f4cb07c755946
briankmbugua <briankmbuguak@gmail.com>
2023-09-29 03:47:12 +0300

    pascal's triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+'''A module for working with Pascal's triangle.
+'''
+
+
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+
+    triangle = []
+    for i in range(n):
+        row = [1]  # The first element of each row is always 1
+        if triangle:
+            last_row = triangle[-1]
+            row.extend([last_row[j] + last_row[j + 1]
+                       for j in range(len(last_row) - 1)])
+            row.append(1)  # The last element of each row is always 1
+        triangle.append(row)
+
+    return triangle
