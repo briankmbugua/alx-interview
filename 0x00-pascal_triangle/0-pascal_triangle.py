0a6cc36f581307414a366bb3f43b509377480363
briankmbugua <briankmbuguak@gmail.com>
2023-09-29 03:54:35 +0300

    pascal's triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -10,12 +10,12 @@ def pascal_triangle(n):
 
     triangle = []
     for i in range(n):
-        row = [1]  # The first element of each row is always 1
+        row = [1]
         if triangle:
             last_row = triangle[-1]
             row.extend([last_row[j] + last_row[j + 1]
                        for j in range(len(last_row) - 1)])
-            row.append(1)  # The last element of each row is always 1
+            row.append(1)
         triangle.append(row)
 
     return triangle
