beefd6ebcc8d8f86f5f9cbfd473772d281aef304
briankmbugua <briankmbuguak@gmail.com>
2023-10-06 02:57:45 +0300

    lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+'''A module for the solution to lockboxes problem.
+'''
+
+
+def canUnlockAll(boxes):
+    '''
+    Returns True if all boxes can be unlocked
+    '''
+    seen_boxes = set([0])
+    unseen_boxes = set(boxes[0])
+
+    while unseen_boxes:
+        boxIdx = unseen_boxes.pop()
+
+        if boxIdx < 0 or boxIdx >= len(boxes):
+            continue
+
+        if boxIdx not in seen_boxes:
+            unseen_boxes.update(boxes[boxIdx])
+            seen_boxes.add(boxIdx)
+
+    return len(boxes) == len(seen_boxes)
+
+
+boxes = [[1], [2], [3], [4], []]
+print(canUnlockAll(boxes))
+
+boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
+print(canUnlockAll(boxes))
+
+boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
+print(canUnlockAll(boxes))
