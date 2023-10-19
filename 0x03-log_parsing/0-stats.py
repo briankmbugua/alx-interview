cf6f015720b2e0f8019fdf66e395e1c3c039d61c
briankmbugua <briankmbuguak@gmail.com>
2023-10-19 17:56:48 +0300

    log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,56 +1,88 @@
 #!/usr/bin/python3
-"""log parsing
-"""
+'''A script for parsing HTTP request logs.
+'''
+import re
 
 
-import sys
-import signal
+def extract_input(input_line):
+    '''Extracts sections of a line of an HTTP request log.
+    '''
+    fp = (
+        r'\s*(?P<ip>\S+)\s*',
+        r'\s*\[(?P<date>\d+\-\d+\-\d+ \d+:\d+:\d+\.\d+)\]',
+        r'\s*"(?P<request>[^"]*)"\s*',
+        r'\s*(?P<status_code>\S+)',
+        r'\s*(?P<file_size>\d+)'
+    )
+    info = {
+        'status_code': 0,
+        'file_size': 0,
+    }
+    log_fmt = '{}\\-{}{}{}{}\\s*'.format(fp[0], fp[1], fp[2], fp[3], fp[4])
+    resp_match = re.fullmatch(log_fmt, input_line)
+    if resp_match is not None:
+        status_code = resp_match.group('status_code')
+        file_size = int(resp_match.group('file_size'))
+        info['status_code'] = status_code
+        info['file_size'] = file_size
+    return info
 
-# Initialize variables to store metrics
-total_file_size = 0
-status_code_counts = {}
-line_count = 0
+
+def print_statistics(total_file_size, status_codes_stats):
+    '''Prints the accumulated statistics of the HTTP request log.
+    '''
+    print('File size: {:d}'.format(total_file_size), flush=True)
+    for status_code in sorted(status_codes_stats.keys()):
+        num = status_codes_stats.get(status_code, 0)
+        if num > 0:
+            print('{:s}: {:d}'.format(status_code, num), flush=True)
 
 
-def print_statistics():
-    print(f"File size: {total_file_size}")
-    for status_code in sorted(status_code_counts.keys(), key=int):
-        print(f"{status_code}: {status_code_counts[status_code]}")
+def update_metrics(line, total_file_size, status_codes_stats):
+    '''Updates the metrics from a given HTTP request log.
+
+    Args:
+        line (str): The line of input from which to retrieve the metrics.
+
+    Returns:
+        int: The new total file size.
+    '''
+    line_info = extract_input(line)
+    status_code = line_info.get('status_code', '0')
+    if status_code in status_codes_stats.keys():
+        status_codes_stats[status_code] += 1
+    return total_file_size + line_info['file_size']
 
 
-# Function to handle keyboard interruption (CTRL + C)
-def signal_handler(sig, frame):
-    print_statistics()
-    sys.exit(0)
+def run():
+    '''Starts the log parser.
+    '''
+    line_num = 0
+    total_file_size = 0
+    status_codes_stats = {
+        '200': 0,
+        '301': 0,
+        '400': 0,
+        '401': 0,
+        '403': 0,
+        '404': 0,
+        '405': 0,
+        '500': 0,
+    }
+    try:
+        while True:
+            line = input()
+            total_file_size = update_metrics(
+                line,
+                total_file_size,
+                status_codes_stats,
+            )
+            line_num += 1
+            if line_num % 10 == 0:
+                print_statistics(total_file_size, status_codes_stats)
+    except (KeyboardInterrupt, EOFError):
+        print_statistics(total_file_size, status_codes_stats)
 
 
-# Set up the signal handler for keyboard interruption
-signal.signal(signal.SIGINT, signal_handler)
-
-for line in sys.stdin:
-    # Split the input line into parts
-    parts = line.split()
-
-    # Check if the line follows the specified format
-    if len(parts) >= 9 and parts[8].isdigit():
-        status_code = parts[8]
-        file_size = int(parts[9])
-
-        # Update total file size
-        total_file_size += file_size
-
-        # Update status code counts
-        if status_code in {"200", "301", "400", "401", "403", "404", "405", "500"}:
-            if status_code in status_code_counts:
-                status_code_counts[status_code] += 1
-            else:
-                status_code_counts[status_code] = 1
-
-        # Increment line count
-        line_count += 1
-
-        # Check if we should print statistics after every 10 lines
-        if line_count % 10 == 0:
-            print_statistics()
-
-print_statistics()
+if __name__ == '__main__':
+    run()
